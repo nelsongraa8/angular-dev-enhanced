ecab7c91b82d21f961c313d91eaf65a4e538aab7
Nelson R. Graña <nelsongraa8@gmail.com>
2024-07-07 21:50:07 +0200

    chore(test): cambio al 90% de covertura

--- config.jest.ts
+++ config.jest.ts
@@ -20,10 +20,10 @@ export default {
   ],
   coverageThreshold: {
     global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: 80,
+      branches: 90,
+      functions: 90,
+      lines: 90,
+      statements: 90,
     },
   },
 };
