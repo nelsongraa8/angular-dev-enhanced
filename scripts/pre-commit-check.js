1adec289dd55d4769305cfc34dbd6a5506397377
Nelson R. Graña <nelsongraa8@gmail.com>
2024-07-14 13:43:24 +0200

    chore(precommit): ajusta script para evitar test

--- scripts/pre-commit-check.js
+++ scripts/pre-commit-check.js
@@ -2,13 +2,19 @@
 const { execSync } = require('child_process');
 
 try {
-  const output = execSync('git diff --cached --name-only', { encoding: 'utf-8' });
+  // Obtiene detalles de los archivos en el área de preparación, incluyendo el estado (A para agregado, D para eliminado)
+  const output = execSync('git diff --cached --name-status', { encoding: 'utf-8' });
 
-  const files = output.split('\n').filter(file => file.startsWith('src/') && file.endsWith('.spec.ts'));
+  // Filtra solo los archivos .spec.ts que están siendo agregados en el directorio src/ o subdirectorios
+  const files = output.split('\n').filter(line => {
+    const [status, file] = line.split('\t');
+    return status === 'A' && file.startsWith('src/') && file.endsWith('.spec.ts');
+  });
 
   if (files.length > 0) {
     console.error('Error: No se permite hacer commit de archivos .spec.ts en src/ o subdirectorios.');
-    console.error(files.join('\n'));
+    // Muestra solo los nombres de los archivos, sin el estado
+    console.error(files.map(line => line.split('\t')[1]).join('\n'));
     process.exit(1); // Sale con error, bloqueando el commit
   }
 } catch (error) {
