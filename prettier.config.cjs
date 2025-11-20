649f651a8f7145be2cfc4ec4c9c50363e254ce0a
Nelson R. Graña <nelsongraa8@gmail.com>
2025-11-20 04:07:19 +0100

    chore(prettier): new config of prettier

---
+++ prettier.config.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+	printWidth: 120, // Máx ancho de línea
+	trailingComma: 'all', // Comas en arrays/objetos multilínea
+	singleQuote: true, // Comillas simples
+	semi: true, // Punto y coma obligatorio
+	tabWidth: 2, // Indentación de 2 espacios
+	useTabs: true, // Espacios en lugar de tabs
+
+	// Extras recomendados
+	bracketSpacing: true, // { foo: bar } en vez de {foo:bar}
+	arrowParens: 'always', // (x) => {} en vez de x => {}
+	endOfLine: 'lf', // Line endings consistentes (útil en equipos multiplataforma)
+};
